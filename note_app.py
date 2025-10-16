17cfff1be2dd9a4c47c7750f9b3539fe2d64c945
8-Bit Michael <mowz2012@gmail.com>
2025-10-15 21:58:55 -0400

    Updated: note_app.py
    I built out the undo() function to pop the note at the top of the stack and place it in either the redo or undo list based on if it was labeled as popped or pushed in it's parameters before the undo function was run.

--- note_app.py
+++ note_app.py
@@ -10,14 +10,25 @@ class NoteApp:
         self.undo_stack.append(('add', text))
         self.redo_stack.clear()
 
-    def delete_last_note(self, text): # Text or an index which would lead you to the note.
+    def delete_last_note(self):
         self.undo_stack.append(('delete', len(self.notes) - 1)) 
         # Storing the index of the deleted node(coming from the top of the stack)
         self.notes.pop() # Pops the top block
         self.redo_stack.clear()
 
+    # The undo stack takes in an action you would like to undo, like deleting a note
+    # from the note stack. The redo stack takes in an action like adding a note back to the
+    # note stack.
     def undo(self):
-        pass
+        node = self.undo_stack.pop() # Assign the popped node to a variable
+        # If the first value in the parameter if you undo that and push it into the redo stack:
+        if node[0] == 'add':
+            self.notes.remove(node[1]) # Remove from the notes list the node index itself.
+            self.redo_stack.append(node) # Append the node to the redo_stack
+        # If the first value is 'delete' you want to add it back to the notes list
+        elif node[0] == 'delete':
+            self.notes.append(node[1]) # Adding it back into the note list
+            self.redo_stack.append(node) # Append the node to the redo_stack  
 
     def redo(self):
         pass
@@ -33,7 +44,13 @@ class NoteApp:
             print(f"{idx + 1}: {note}") # Print the numbered notes
 
 app = NoteApp() # Initalize the app
-app.add_note("First note") # Create the first note
-app.show_notes()
-app.undo_stack("First note")
-app.show_notes()
+
+def add_note_test():
+    print("=== Test: Note Initialization ===")
+    app.add_note("First note") # Create the first note
+    app.show_notes()
+
+def undo_note_test():
+    print("=== Test: Undo Note ===")
+    app.undo() # Undo the last note added
+    app.show_notes()

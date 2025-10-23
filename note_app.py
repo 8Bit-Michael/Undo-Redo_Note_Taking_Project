b40ce05bf11511b5736dd5c99b5d2d8c147415b4
8-Bit Michael <mowz2012@gmail.com>
2025-10-22 21:58:30 -0400

    Updated: note_app.py
    I updated this module to return certain responses and include some error handling in different conditions, like there being an empty list that you couldn't perform certain operations on, to now just returning an error message when empty.

--- note_app.py
+++ note_app.py
@@ -1,53 +1,79 @@
+import os
+
 class NoteApp:
+    
     def __init__(self, undo_stack, redo_stack, notes):
         self.redo_stack = [] # Redo operations
         self.undo_stack = [] # Undo operations
         self.notes = [] # The current notes
 
     def add_note(self, text):
-        self.notes.append(text)
-        self.undo_stack.append(('add', text))
-        self.redo_stack.clear()
+        if not self.notes:
+            return "No notes to delete"
+        else:
+            self.notes.append(text)
+            self.undo_stack.append(('add', text))
+            self.redo_stack.clear()
+            return f"{text} added to notes"
 
     def delete_last_note(self):
-        self.undo_stack.append(('delete', len(self.notes) - 1)) 
-        # Storing the index of the deleted node(coming from the top of the stack)
-        self.notes.pop() # Pops the top block
-        self.redo_stack.clear()
+        if not self.notes:
+            return "No notes to delete"
+        else:
+            self.undo_stack.append(('delete', len(self.notes) - 1)) 
+            # Storing the index of the deleted node(coming from the top of the stack)
+            self.notes.pop() # Pops the top block
+            self.redo_stack.clear()
+            return f"Last note ({self.notes[-1]}) deleted"
 
     # The undo stack takes in an action you would like to undo, like deleting a note
     # from the note stack. The redo stack takes in an action like adding a note back to the
     # note stack.
     def undo(self):
-        node = self.undo_stack.pop() # Assign the popped node from the undo stack to a variable
-        # If the second value in the parameter is 'add':
-        if node[0] == 'add': # Undo that and push it into the redo stack
-            self.notes.remove(node[1]) # Remove from the notes list the node index itself.
-            self.redo_stack.append(node) # Append the node to the redo_stack
-        # If the first value is 'delete' you want to add it back to the notes list
-        elif node[0] == 'delete':
-            self.notes.append(node[1]) # Adding it back into the note list
-            self.redo_stack.append(node) # Append the node to the redo_stack  
+        if self.undo_stack is None or len(self.undo_stack) == 0:
+            return "No actions to undo"
+        else:
+            node = self.undo_stack.pop() # Assign the popped node from the undo stack to a variable
+            # If the second value in the parameter is 'add':
+            if node[0] == 'add': # Undo that and push it into the redo stack
+                self.notes.remove(node[1]) # Remove from the notes list the node index itself.
+                self.redo_stack.append(node) # Append the node to the redo_stack
+            # If the first value is 'delete' you want to add it back to the notes list
+            elif node[0] == 'delete':
+                self.notes.append(node[1]) # Adding it back into the note list
+                self.redo_stack.append(node) # Append the node to the redo_stack  
+            return f"Undid action: {node[0]}"
 
     def redo(self):
-        node = self.redo_stack.pop() # Erase this node from the redo stack
-        if node[0] == 'add': # If you want to redo adding a node
-            self.notes.append(node) # Add it back to the notes list
-        elif node[0] == 'delete': # if you change your mind and decide to delete something
-            self.undo_stack.append(node) # push the action back onto undo stack
+        if self.redo_stack is None or len(self.redo_stack) == 0:
+            return "No actions to redo"
+        else:
+            node = self.redo_stack.pop() # Erase this node from the redo stack
+            if node[0] == 'add': # If you want to redo adding a node
+                self.notes.append(node) # Add it back to the notes list
+            elif node[0] == 'delete': # if you change your mind and decide to delete something
+                self.undo_stack.append(node) # push the action back onto undo stack
+            return f"Redid action: {node[0]}"
 
     def save_to_file(self, filename):
-        with open(filename, 'w') as f: # Open a file in which you can write
-            for idx in self.notes: # Upload each note
-                f.write(f"{idx}\n") # Write each node into that file
-        return "Notes saved successfully"
+        if not os.path.exists(filename): # Check if the file does not exist
+            return f"{filename} does not exist"
+        else: # If the file does exist
+            with open(filename, 'w') as f: # Open a file in which you can write
+                for idx in self.notes: # Upload each note
+                    f.write(f"{idx}\n") # Write each node into that file
+            return "Notes saved successfully"
 
     def load_from_file(self, filename):
-        with open(filename, 'r') as f: # This time you're reading from the file
-            loaded_list = [line.strip() for line in f] # Take out the data, strip it, and then make a list
-            self.notes = loaded_list # Assign the loaded list to the notes list
-            self.undo_stack.clear() # Clear the undo stack
-            self.redo_stack.clear() # Clear the redo stack
+        if not os.path.exists(filename): # Check if the file does not exist
+            return f"{filename} does not exist"
+        else: # Otherwise
+            with open(filename, 'r') as f: # This time you're reading from the file
+                loaded_list = [line.strip() for line in f] # Take out the data, strip it, and then make a list
+                self.notes = loaded_list # Assign the loaded list to the notes list
+                self.undo_stack.clear() # Clear the undo stack
+                self.redo_stack.clear() # Clear the redo stack
+            return "Notes successfully loaded"
 
     def show_notes(self):
         for idx, note in enumerate(self.notes): # Loops through the notes with their indexes
